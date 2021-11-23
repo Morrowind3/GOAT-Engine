03d5af7e95db5a8b925b3fd659ea18fdf0824563
MikeStutje <mikestutje@gmail.com>
2021-11-23 13:56:54 +0100

    suffering 2.0

--- src/Libraries/BOX2D/Dynamics/Contacts/b2ChainAndCircleContact.cpp
+++ src/Libraries/BOX2D/Dynamics/Contacts/b2ChainAndCircleContact.cpp
@@ -16,11 +16,12 @@
 * 3. This notice may not be removed or altered from any source distribution.
 */
 
-#include "Box2D/Dynamics/Contacts/b2ChainAndCircleContact.h"
-#include "Box2D/Common/b2BlockAllocator.h"
-#include "Box2D/Dynamics/b2Fixture.h"
-#include "Box2D/Collision/Shapes/b2ChainShape.h"
-#include "Box2D/Collision/Shapes/b2EdgeShape.h"
+#include "./Common/b2BlockAllocator.h"
+#include "./Dynamics/b2Fixture.h"
+#include "./Dynamics/Contacts/b2ChainAndCircleContact.h"
+#include "./Collision/Shapes/b2EdgeShape.h"
+#include "./Collision/Shapes/b2ChainShape.h"
+
 
 #include <new>
 
