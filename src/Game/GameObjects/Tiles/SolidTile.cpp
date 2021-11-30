4cc805e90b7f581cf7c96676a38a80249076fd26
StijnvLoon <stijnvanloon@home.nl>
2021-11-30 12:45:40 +0100

    added lava tile, clean up other tiles

--- src/Game/GameObjects/Tiles/SolidTile.cpp
+++ src/Game/GameObjects/Tiles/SolidTile.cpp
@@ -1,10 +1,9 @@
 #include "SolidTile.hpp"
-#include "../../Behaviors/Tiles/SolidTileBehavior.hpp"
 #include "../../../Engine/API/GameObjects/GameComponents/Collision/BoxCollider.hpp"
 #include "../../Keys.hpp"
 
 SolidTile::SolidTile(const std::string& sprite, Transform transform, bool active)
-        : Tile(sprite, std::make_shared<SolidTileBehavior>(*this, active), transform, active) {
+        : Tile(sprite, transform, active) {
     tags.insert(std::make_pair(Keys::SOLID, true));
     rigidBody = {0.0f, 0.0f, BodyType::STATIC, true};
     collider = BoxCollider(21 * transform.scaleWidth,21 * transform.scaleHeight, true);
