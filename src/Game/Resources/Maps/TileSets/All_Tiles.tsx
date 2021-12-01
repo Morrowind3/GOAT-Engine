eaced078f417ea185d3153eaa6c92caa4a3bab9b
StijnvLoon <stijnvanloon@home.nl>
2021-12-01 19:45:49 +0100

    fix

---
+++ src/Game/Resources/Maps/TileSets/All_Tiles.tsx
@@ -0,0 +1,187 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="All_Tiles" tilewidth="21" tileheight="21" tilecount="61" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="21" height="21" source="../../Sprites/landscapes/grass/grass_slab_flat.png"/>
+ </tile>
+ <tile id="1">
+  <image width="21" height="21" source="../../Sprites/landscapes/grass/grass_slab_normal.png"/>
+ </tile>
+ <tile id="2">
+  <image width="21" height="21" source="../../Sprites/landscapes/grass/grass_ground_empty.png"/>
+ </tile>
+ <tile id="3">
+  <image width="21" height="21" source="../../Sprites/landscapes/grass/grass_ground_end_left.png"/>
+ </tile>
+ <tile id="4">
+  <image width="21" height="21" source="../../Sprites/landscapes/grass/grass_ground_end_right.png"/>
+ </tile>
+ <tile id="5">
+  <image width="21" height="21" source="../../Sprites/landscapes/grass/grass_ground_flat.png"/>
+ </tile>
+ <tile id="6">
+  <image width="21" height="21" source="../../Sprites/landscapes/grass/grass_ground_normal.png"/>
+ </tile>
+ <tile id="7">
+  <image width="21" height="21" source="../../Sprites/landscapes/grass/grass_slab_end_left.png"/>
+ </tile>
+ <tile id="8">
+  <image width="21" height="21" source="../../Sprites/landscapes/grass/grass_slab_end_right.png"/>
+ </tile>
+ <tile id="9">
+  <image width="21" height="21" source="../../Sprites/landscapes/lava/lava_slab_flat.png"/>
+ </tile>
+ <tile id="10">
+  <image width="21" height="21" source="../../Sprites/landscapes/lava/lava_slab_normal.png"/>
+ </tile>
+ <tile id="11">
+  <image width="21" height="21" source="../../Sprites/landscapes/lava/lava_ground_empty.png"/>
+ </tile>
+ <tile id="12">
+  <image width="21" height="21" source="../../Sprites/landscapes/lava/lava_ground_end_left.png"/>
+ </tile>
+ <tile id="13">
+  <image width="21" height="21" source="../../Sprites/landscapes/lava/lava_ground_end_right.png"/>
+ </tile>
+ <tile id="14">
+  <image width="21" height="21" source="../../Sprites/landscapes/lava/lava_ground_flat.png"/>
+ </tile>
+ <tile id="15">
+  <image width="21" height="21" source="../../Sprites/landscapes/lava/lava_ground_normal.png"/>
+ </tile>
+ <tile id="16">
+  <image width="21" height="21" source="../../Sprites/landscapes/lava/lava_slab_end_left.png"/>
+ </tile>
+ <tile id="17">
+  <image width="21" height="21" source="../../Sprites/landscapes/lava/lava_slab_end_right.png"/>
+ </tile>
+ <tile id="18">
+  <image width="21" height="21" source="../../Sprites/landscapes/snow/snow_slab_flat.png"/>
+ </tile>
+ <tile id="19">
+  <image width="21" height="21" source="../../Sprites/landscapes/snow/snow_slab_normal.png"/>
+ </tile>
+ <tile id="20">
+  <image width="21" height="21" source="../../Sprites/landscapes/snow/snow_ground_empty.png"/>
+ </tile>
+ <tile id="21">
+  <image width="21" height="21" source="../../Sprites/landscapes/snow/snow_ground_end_left.png"/>
+ </tile>
+ <tile id="22">
+  <image width="21" height="21" source="../../Sprites/landscapes/snow/snow_ground_end_right.png"/>
+ </tile>
+ <tile id="23">
+  <image width="21" height="21" source="../../Sprites/landscapes/snow/snow_ground_flat.png"/>
+ </tile>
+ <tile id="24">
+  <image width="21" height="21" source="../../Sprites/landscapes/snow/snow_ground_normal.png"/>
+ </tile>
+ <tile id="25">
+  <image width="21" height="21" source="../../Sprites/landscapes/snow/snow_slab_end_left.png"/>
+ </tile>
+ <tile id="26">
+  <image width="21" height="21" source="../../Sprites/landscapes/snow/snow_slab_end_right.png"/>
+ </tile>
+ <tile id="27">
+  <image width="21" height="21" source="../../Sprites/landscapes/stone/stone_slab_flat.png"/>
+ </tile>
+ <tile id="28">
+  <image width="21" height="21" source="../../Sprites/landscapes/stone/stone_slab_normal.png"/>
+ </tile>
+ <tile id="29">
+  <image width="21" height="21" source="../../Sprites/landscapes/stone/stone_ground_empty.png"/>
+ </tile>
+ <tile id="30">
+  <image width="21" height="21" source="../../Sprites/landscapes/stone/stone_ground_end_left.png"/>
+ </tile>
+ <tile id="31">
+  <image width="21" height="21" source="../../Sprites/landscapes/stone/stone_ground_end_right.png"/>
+ </tile>
+ <tile id="32">
+  <image width="21" height="21" source="../../Sprites/landscapes/stone/stone_ground_flat.png"/>
+ </tile>
+ <tile id="33">
+  <image width="21" height="21" source="../../Sprites/landscapes/stone/stone_ground_normal.png"/>
+ </tile>
+ <tile id="34">
+  <image width="21" height="21" source="../../Sprites/landscapes/stone/stone_slab_end_left.png"/>
+ </tile>
+ <tile id="35">
+  <image width="21" height="21" source="../../Sprites/landscapes/stone/stone_slab_end_right.png"/>
+ </tile>
+ <tile id="36">
+  <image width="21" height="21" source="../../Sprites/enemies/bird/bird_idle_2.png"/>
+ </tile>
+ <tile id="44">
+  <image width="21" height="21" source="../../Sprites/enemies/goat/goat_idle_1.png"/>
+ </tile>
+ <tile id="45">
+  <image width="21" height="21" source="../../Sprites/enemies/snake/snake_popup.png"/>
+ </tile>
+ <tile id="46">
+  <image width="21" height="21" source="../../Sprites/enemies/snowball/snowball.png"/>
+ </tile>
+ <tile id="47">
+  <image width="21" height="21" source="../../Sprites/utils/flag/Flag.png"/>
+ </tile>
+ <tile id="48">
+  <image width="21" height="21" source="../../Sprites/utils/items/Jägerpleister.png"/>
+ </tile>
+ <tile id="49">
+  <image width="21" height="21" source="../../Sprites/utils/lakes/lake_lava.png"/>
+ </tile>
+ <tile id="50">
+  <image width="21" height="21" source="../../Sprites/utils/lakes/lake_ice.png"/>
+ </tile>
+ <tile id="51">
+  <image width="21" height="21" source="../../Sprites/utils/tree/tree_leaves_side_right_2.png"/>
+ </tile>
+ <tile id="52">
+  <image width="21" height="21" source="../../Sprites/utils/tree/tree_leaves_top_1.png"/>
+ </tile>
+ <tile id="53">
+  <image width="21" height="21" source="../../Sprites/utils/tree/tree_leaves_top_2.png"/>
+ </tile>
+ <tile id="54">
+  <image width="21" height="21" source="../../Sprites/utils/tree/tree_middle_branch_left.png"/>
+ </tile>
+ <tile id="55">
+  <image width="21" height="21" source="../../Sprites/utils/tree/tree_middle_branch_right.png"/>
+ </tile>
+ <tile id="56">
+  <image width="21" height="21" source="../../Sprites/utils/tree/tree_middle_normal.png"/>
+ </tile>
+ <tile id="57">
+  <image width="21" height="21" source="../../Sprites/utils/tree/tree_sapling.png"/>
+ </tile>
+ <tile id="58">
+  <image width="21" height="21" source="../../Sprites/utils/tree/tree_stump_bottom.png"/>
+ </tile>
+ <tile id="59">
+  <image width="21" height="21" source="../../Sprites/utils/tree/tree_stump_top.png"/>
+ </tile>
+ <tile id="60">
+  <image width="21" height="21" source="../../Sprites/utils/tree/tree_branch.png"/>
+ </tile>
+ <tile id="61">
+  <image width="21" height="21" source="../../Sprites/utils/tree/tree_branch_left.png"/>
+ </tile>
+ <tile id="62">
+  <image width="21" height="21" source="../../Sprites/utils/tree/tree_branch_right.png"/>
+ </tile>
+ <tile id="63">
+  <image width="21" height="21" source="../../Sprites/utils/tree/tree_leaves_middle.png"/>
+ </tile>
+ <tile id="64">
+  <image width="21" height="21" source="../../Sprites/utils/tree/tree_leaves_side_left_1.png"/>
+ </tile>
+ <tile id="65">
+  <image width="21" height="21" source="../../Sprites/utils/tree/tree_leaves_side_left_2.png"/>
+ </tile>
+ <tile id="66">
+  <image width="21" height="21" source="../../Sprites/utils/tree/tree_leaves_side_right_1.png"/>
+ </tile>
+ <tile id="67">
+  <image width="21" height="21" source="../../Sprites/waste/waste_pizzabox.png"/>
+ </tile>
+</tileset>
