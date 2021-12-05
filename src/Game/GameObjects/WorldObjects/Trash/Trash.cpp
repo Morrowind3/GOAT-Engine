f63be5a0b861c54d3d936d444252483e74f22add
Harley Rombout <hnmrombo@avans.nl>
2021-12-05 16:08:22 +0100

    random range crashfix and timerline pause fix

--- src/Game/GameObjects/WorldObjects/Trash/Trash.cpp
+++ src/Game/GameObjects/WorldObjects/Trash/Trash.cpp
@@ -26,7 +26,7 @@ Trash::Trash(Transform transform, bool active) : GameObject(transform, active) {
 
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::default_random_engine generator(seed);
-    std::uniform_int_distribution<int> random(0,SPRITE_OPTIONS.size());
+    std::uniform_int_distribution<int> random(0,SPRITE_OPTIONS.size()-1);
 
 
 
