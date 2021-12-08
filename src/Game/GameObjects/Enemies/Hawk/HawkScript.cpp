71b7ecba8083a5b7c56fef2c4f83debd6a2c0a7a
StijnvLoon <stijnvanloon@home.nl>
2021-12-08 15:10:53 +0100

    work on behaviors, but cant make anything usefull with current physics

--- src/Game/GameObjects/Enemies/Hawk/HawkScript.cpp
+++ src/Game/GameObjects/Enemies/Hawk/HawkScript.cpp
@@ -1,33 +1,79 @@
+#include <chrono>
+#include <random>
 #include "HawkScript.hpp"
 #include "../../../Keys.hpp"
 
-HawkScript::HawkScript(Hawk& self, bool active) : Script(active), _self(self), _startingPos(self.transform.position) {
-    _self.transform.flip = Engine::FLIP::FLIP_HORIZONTAL;
+void HawkScript::onTriggerEnter2D(GameObject& other) {
+    //if going down and touching a tile or player, go back up
+    if (_direction == DOWN && (other.hasTag(Keys::TILE) || other.hasTag(Keys::PLAYER))) {
+        _direction = UP;
+    }
 }
 
 void HawkScript::onUpdate(double deltaTime) {
+    this->updateDirection();
+    this->updatePosition();
+    this->updateSprite();
+}
 
-    if(_direction == RIGHT) {
-        _self.transform.position.x += 0.8;
-    }
-    if(_direction == LEFT) {
-        _self.transform.position.x -= 0.8;
-    }
+void HawkScript::updateDirection() {
 
-    // switch direction
-    if(_self.transform.position.x < _startingPos.x || _self.transform.position.x - _startingPos.x > 400) {
-        if(_direction == RIGHT) {
-            _direction = LEFT;
-            _self.transform.flip = Engine::FLIP::FLIP_NONE;
-        } else {
-            _direction = RIGHT;
-            _self.transform.flip = Engine::FLIP::FLIP_HORIZONTAL;
+    //if going up, and reaching original height, resume idling
+    if(_direction == UP && _self.transform.position.y <= _startingPos.y) {
+        _direction = RIGHT;
+    } else if(_direction == DOWN && (_self.transform.position.y - _startingPos.y > 200)) {
+        _direction = UP;
+    };
+
+    //if idling, DIVE_CHANGE change to dive
+    if(_direction == LEFT || _direction == RIGHT) {
+        if (HawkScript::getRandomBetween(0, 100) < DIVE_CHANGE) {
+            _direction = DOWN;
         }
     }
 
-    //switch move sprite
-    if(_updateCounter%50 == 1) {
-        if(_self.sprites.at(Keys::MOVE1).active) {
+    //if flying furter than scope, change direction
+    if (_direction == RIGHT && (_self.transform.position.x - _startingPos.x >= FLYING_SCOPE)) {
+        _direction = LEFT;
+    }
+    if (_direction == LEFT && _self.transform.position.x <= _startingPos.x) {
+        _direction = RIGHT;
+    }
+}
+
+void HawkScript::updatePosition() {
+
+    switch (_direction) {
+        case RIGHT:
+//            _self.transform.position.x += SPEED_IDLE;
+            _self.rigidBody.forceX += SPEED_IDLE;
+            break;
+        case LEFT:
+//            _self.transform.position.x -= SPEED_IDLE;
+            _self.rigidBody.forceX -= SPEED_IDLE;
+            break;
+        case DOWN:
+//            _self.transform.position.y += SPEED_DIVING;
+            _self.rigidBody.forceY -= SPEED_DIVING;
+            break;
+        case UP:
+//            _self.transform.position.y -= SPEED_RISING;
+            _self.rigidBody.forceY += SPEED_RISING;
+            break;
+    }
+}
+
+void HawkScript::updateSprite() {
+
+    if(_direction == LEFT) {
+        _self.transform.flip = FLIP::FLIP_NONE;
+    }
+    if(_direction == RIGHT) {
+        _self.transform.flip = FLIP::FLIP_HORIZONTAL;
+    }
+
+    if (_updateCounter % 50 == 1) {
+        if (_self.sprites.at(Keys::MOVE1).active) {
             _self.sprites.at(Keys::MOVE1).active = false;
             _self.sprites.at(Keys::MOVE2).active = true;
         } else {
@@ -36,4 +82,26 @@ void HawkScript::onUpdate(double deltaTime) {
         }
     }
     _updateCounter++;
+
+    switch (_direction) {
+        case RIGHT:
+            _debug.log("RIGHT");
+            break;
+        case LEFT:
+            _debug.log("LEFT");
+            break;
+        case DOWN:
+            _debug.log("DOWN");
+            break;
+        case UP:
+            _debug.log("UP");
+            break;
+    }
+}
+
+int HawkScript::getRandomBetween(int from, int to) {
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine generator(seed);
+    std::uniform_int_distribution<int> random(from, to);
+    return random(generator);
 }
--- src/Game/GameObjects/Enemies/Hawk/HawkScript.hpp
+++ src/Game/GameObjects/Enemies/Hawk/HawkScript.hpp
@@ -4,25 +4,42 @@
 #include "../../../../Engine/API/GameObjects/GameObject.hpp"
 #include "../../../../Engine/API/GameObjects/GameComponents/Script/Script.hpp"
 #include "Hawk.hpp"
+#include "../../../../Engine/Utilities/Debug.hpp"
 #include <ctime>
 
 using namespace Engine;
 
-enum Direction {
+enum HawkDirection {
     LEFT,
-    RIGHT
+    RIGHT,
+    DOWN,
+    UP
 };
 
 class HawkScript : public Script {
 public:
-    HawkScript(Hawk& self, bool active);
+    HawkScript(Hawk& self, bool active) : Script(active), _self(self), _startingPos(self.transform.position) {};
     void onUpdate(double deltaTime);
+    void onTriggerEnter2D(GameObject &other);
+
 private:
     Hawk& _self;
     Point _startingPos;
-    Direction _direction = RIGHT;
+    HawkDirection _direction = RIGHT;
     int _updateCounter{0};
-//    std::time_t _lastMoveTime = std::time(0);
+
+    int FLYING_SCOPE{400};  //amount of pixels
+    int DIVE_CHANGE{1}; //number between 0-100
+    float SPEED_IDLE{0.4};
+    float SPEED_DIVING{0.2};   //2.6
+    float SPEED_RISING{1.4};
+
+    Debug& _debug = Debug::getInstance();
+
+    void updateDirection();
+    void updatePosition();
+    void updateSprite();
+    static int getRandomBetween(int from, int to);
 
 };
 
