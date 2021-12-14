33ecd0381205071bb5f60172bdcaf37f3b93af7e
Julian Maas <julian-maas@outlook.com>
2021-12-14 14:44:36 +0100

    More "empty" tiles where you would expect them
    Aka no more floating mountain

--- src/Game/Scenes/Etappes/Scene_EtappeFive.cpp
+++ src/Game/Scenes/Etappes/Scene_EtappeFive.cpp
@@ -5,8 +5,8 @@
 #include "../../GameObjects/Scenes/Etappes/EtappeFiveScene/Object_EtappeFiveManager.hpp"
 
 
-Scene_EtappeFive::Scene_EtappeFive() : SceneTemplate_Etappe(Keys::ETAPPE_ONE, {99999999,9999999}, // TODO: Dimensions
-                                                             "Maps/Etappe5.map", 21, 50, 250, 4, 0, -3000) {
+Scene_EtappeFive::Scene_EtappeFive() : SceneTemplate_Etappe(Keys::ETAPPE_FIVE, {99999999,9999999}, // TODO: Dimensions
+                                                             "Maps/Etappe5.map", 21, 50, 256, 4, 0, -3000) {
     gameObjects.emplace_back(std::make_shared<Object_TimerLine>(6,Transform{{0,-3500},LAYER::TIMER_LINE, 0, 0,3, 80}, true ));
     gameObjects.emplace_back(std::make_shared<Object_EtappeFiveManager>(
             Transform{Point{0, -242}, LAYER::PARALLAX_BACKGROUND, 0,0, 20, 20}, true));
