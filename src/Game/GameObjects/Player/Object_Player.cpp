e732cb7ad8fe20f6951dd60cd0c91a6738d7bd44
Julian Maas <julian-maas@outlook.com>
2021-12-19 15:07:17 +0100

    Pretty certain we'd get complaints if I didn't remove this
    Because this object is showcased in one of our documents as prime example

--- src/Game/GameObjects/Player/Object_Player.cpp
+++ src/Game/GameObjects/Player/Object_Player.cpp
@@ -18,7 +18,7 @@ Object_Player::Object_Player(Transform transform, bool active) : GameObject(tran
     rigidBody = RigidBody(75.0f, 8.5f, BodyType::DYNAMIC, true);
     collider = OvalCollider(18.5f, 0.8f, 1.55f,0.2f,0, true);
     // Logic
-    auto& playerBehavior = behaviors.insert(std::make_pair(Keys::BEHAVIOR,std::make_shared<Behavior_Player>(*this, true))).first->second;
+    behaviors.insert(std::make_pair(Keys::BEHAVIOR,std::make_shared<Behavior_Player>(*this, true)));
     // Render
     auto& idleSprite = sprites.insert(std::make_pair(Keys::IDLE, Sprite_PlayerIdle{false})).first->second;
     auto& moveSprite1 = sprites.insert(std::make_pair(Keys::MOVE1, Sprite_PlayerMove1{false})).first->second;
@@ -26,11 +26,11 @@ Object_Player::Object_Player(Transform transform, bool active) : GameObject(tran
     auto& moveSprite3 = sprites.insert(std::make_pair(Keys::MOVE3, Sprite_PlayerMove3{false})).first->second;
     auto& jumpSprite = sprites.insert(std::make_pair(Keys::JUMP, Sprite_PlayerJump{false})).first->second;
 	auto& pickupSprite = sprites.insert(std::make_pair(Keys::TRASH, Sprite{"Sprites/player/player_pickup.png", false})).first->second;
-    auto& walkingAnimator = animators.insert(std::make_pair(Keys::WALKING_ANIMATOR, Animator_PlayerWalking{moveSprite1, moveSprite2, moveSprite3, moveSprite2, false})).first->second;
+    animators.insert(std::make_pair(Keys::WALKING_ANIMATOR, Animator_PlayerWalking{moveSprite1, moveSprite2, moveSprite3, moveSprite2, false})).first->second;
     // Audio
-    auto& jumpSound = audioSources.insert(std::make_pair(Keys::JUMP_SFX,Sound_Jump{1,100,false,true})).first->second;
-    auto& walkSound = audioSources.insert(std::make_pair(Keys::WALK_SFX_A,Sound_Walk{1,100,false,true})).first->second;
-    auto& walkSoundAlt = audioSources.insert(std::make_pair(Keys::WALK_SFX_B,Sound_WalkAlt{1,100,false,true})).first->second;
-    auto& oofSound = audioSources.insert(std::make_pair(Keys::DAMAGE_SFX,Sound_Oof{1,100,false,true})).first->second;
-    auto& deathSound = audioSources.insert(std::make_pair(Keys::DEATH_SFX,Sound_Death{1,100,false,true})).first->second;
+    audioSources.insert(std::make_pair(Keys::JUMP_SFX,Sound_Jump{1,100,false,true}));
+    audioSources.insert(std::make_pair(Keys::WALK_SFX_A,Sound_Walk{1,100,false,true}));
+    audioSources.insert(std::make_pair(Keys::WALK_SFX_B,Sound_WalkAlt{1,100,false,true}));
+    audioSources.insert(std::make_pair(Keys::DAMAGE_SFX,Sound_Oof{1,100,false,true}));
+    audioSources.insert(std::make_pair(Keys::DEATH_SFX,Sound_Death{1,100,false,true}));
 }
